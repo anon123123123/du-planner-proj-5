e23e1862034f1293b8b5d1d91025b7d97efa8381
anon123123123 <cditodavis96@gmail.com>
2022-01-24 21:48:25 -0700

    Working on LS handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 const closeModal = $("#close-modal")
 const planClassSelect = $(".plan-event")
 let targetEventId = ""
+let textData = ""
 
 // Call moment and get hour 
 let now = moment();
@@ -16,7 +17,17 @@ if (hour >= 12) {
 $("#currentDay").text(`${now.format('dddd, MMMM Do YYYY')}`)
 
 const writeLStoEvents = async () => {
-    // Check if LS id matches data id 
+    let lsObj = JSON.parse(localStorage.plans)
+    const idArray = ["t9AM", "t10AM", "t11AM", "t12PM", "t1PM", "t2PM", "t3PM", "t4PM", "t5PM"]
+    idArray.forEach(element => {
+        if(lsObj[element]){
+            console.log(element)
+            let matchedId = $(".plan-event[data-id=" + element + "]")[0]
+            matchedId.innerText = lsObj[element]
+            console.log(matchedId)
+        }
+    })
+    // Check if LS id matches data id  $(".plan-event[data-id=9AM]")[0]
 }
 
 // Checks if LS exists
@@ -26,28 +37,41 @@ if(localStorage.plans) {
 
 // Local storage handler 
 const addToLS = async (textData, x) => {
-    let targetId = x
     let plans = {}
-    plans[targetId] = textData 
     if(localStorage.plans) {
+        let targetId = x
+        plans[targetId] = textData 
         let old = JSON.parse(localStorage.getItem('plans'))
         let newObj = {...old,...plans}
         localStorage.setItem('plans',JSON.stringify(newObj))
     } else {
+        let targetId = x
+        plans[targetId] = textData 
         localStorage.setItem('plans',JSON.stringify(plans))
     }
 }
 
 
 // Adds text to event upon close of modal
-const addTextFromModal = async (e) => {
+function addTextFromModal (e)  {
     let eventTextEl = $('.plan-event[data-id="' + targetEventId +'"]')[0]
-    let textData = $("#modal-textarea")[0].value
+    textData = $("#modal-textarea")[0].value
     eventTextEl.innerText = textData
-    addToLS(textData, targetEventId) 
+    $("#modal-textarea")[0].value = "" 
+}
+
+const saveData =  async (e) => {
+    let eventElement = await $(e.target).parent().siblings(".plan-event")
+    console.log(eventElement)
+    targetEventId = await eventElement.attr('data-id')
+    console.log(targetEventId)
+    // textData = await eventElement[0].value
+    // console.log(textData) TO DO FIX
+    // addToLS(textData, targetEventId)
 }
 // Listen for close button on event
 closeModal.on('click', addTextFromModal)
+$('.plan-save').on('click', saveData)
 
 // Hacky way to grab data id 
 planClassSelect.on('click', function(e) {
